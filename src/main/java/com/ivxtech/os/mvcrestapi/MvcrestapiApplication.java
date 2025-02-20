688bbedcebee9a260e045a60ff4a424c9b94b01a
omer salmanoglu <omer.salmanoglu@gmail.com>
2025-02-20 10:12:24 +0300

    first commit

---
+++ src/main/java/com/ivxtech/os/mvcrestapi/MvcrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.ivxtech.os.mvcrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivxtech/os/mvcrestapi/MvcrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivxtech.os.mvcrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
